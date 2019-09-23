c595f15f0eed3e973658e23a6408e153b66d6632
LajiYang <842458343@qq.com>
2019-09-23 19:03:44 +0800

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ App({
     wx.login({
       success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log('11111111')
+        console.log('11111111');
       }
     })
     // 获取用户信息

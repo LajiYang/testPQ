33ffe7b84ab751a53fb12cc0496e05116c57be17
liyang <liyang@yaoguang.net>
2019-09-23 15:01:32 +0800

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function() {
+  onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -10,6 +10,8 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
+        var a = 1 + 1
       }
     })
     // 获取用户信息

c60744acf6b76ead83e43b734b56ca33b4509636
liyang <liyang@yaoguang.net>
2019-09-23 17:35:07 +0800

    app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ App({
       success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         console.log('11111111')
+        console.log('22222222')
       }
     })
     // 获取用户信息
